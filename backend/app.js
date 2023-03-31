0437f3a76d962612e3d4e805ebf2021ce714f41b
Olivier <olivier.bent@gmail.com>
2023-03-31 13:12:49 +0200

    connection to sql database

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,20 @@ const morgan = require("morgan");
 //creation de l'application express
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+    );
+    next();
+  });
+
+
 app.use(morgan("dev"));
 
 //export de l'application
---
+++ backend/dbConnection.js
@@ -0,0 +1,20 @@
+//import variable d'environnement
+require("dotenv").config();
+
+const mysql = require("mysql");
+
+const database = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+database.connect(function (err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
+});
+
+
+
+
+module.exports = database;
