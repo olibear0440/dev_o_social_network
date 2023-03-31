2f2a4beebb340efdee716d383a43066e739cce98
Olivier <olivier.bent@gmail.com>
2023-03-31 14:37:28 +0200

    rajout packages de securisations

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
 //import package express
 const express = require("express");
+//import helmet (protection en-tete http)
+const helmet = require("helmet");
+//import xss-clean (nettoie les inputs users des requetes et des parametres url)
+const xss = require("xss-clean");
 
 //import variable d'environnement
 require("dotenv").config();
+
+const path = require("path");
+const cors = require("cors");
 
 //import morgan
 const morgan = require("morgan");
@@ -11,17 +18,17 @@ const morgan = require("morgan");
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,POST,PUT,DELETE,PATCH,OPTIONS"
-    );
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+  );
+  next();
+});
 
 //import des routes
 const registerRoutes = require("./routes/register");
@@ -29,6 +36,9 @@ const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
 const commentRoutes = require("./routes/comment");
 
+app.use(xss());
+app.use(helmet());
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
@@ -37,6 +47,9 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 app.use("/registers", registerRoutes);
 app.use("/comments", commentRoutes);
+
+//middleware dossier images
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 //export de l'application
 module.exports = app;
